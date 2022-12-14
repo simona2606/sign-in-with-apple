f8da4a5301fe43f7bd080d1b5fdb4f98ace704ca
simona2606 <simonaettari26@gmail.com>
2022-12-14 12:32:48 +0100

    Initial Commit

---
+++ SignInWithApple/SignInWithAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInWithAppleApp.swift
+//  SignInWithApple
+//
+//  Created by Simona Ettari on 14/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SignInWithAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
